e11e6250a16aad727ad701c7b9cb7db8925edb1f
Rene Montoya <rmontoya.we@gmail.com>
2020-12-10 23:19:08 -0800

    finished registration and login methods, need to work on logout

--- login_app/models.py
+++ login_app/models.py
@@ -1,6 +1,23 @@
 from django.db import models
+import re
 
 # Create your models here.
+class myManager(models.Manager):
+    def my_validator(self, postData):
+        errors = {}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = "Invalid email address!"
+        if postData['password'] != postData['confirm_pw']:
+            errors['confirm_pw'] = "Passwords do not match!"
+        if len(postData['first_name']) < 2:
+            errors['first_name'] = "First name must be at least 2 characters"
+        if len(postData['last_name']) < 2:
+            errors['last_name'] = "Last name must be at least 2 characters"
+        if len(postData['password']) < 8:
+            errors['password'] = "Password must be at least 8 characters"
+        return errors
+
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -8,3 +25,4 @@ class User(models.Model):
     password = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = myManager()
--- login_app/views.py
+++ login_app/views.py
@@ -1,5 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
+from django.contrib import messages
+import bcrypt
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    context = {
+        'allUsers': User.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def register(request):
+    password = request.POST['password']
+    pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    print('hash', User.objects.all())
+    User.objects.create(
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        email = request.POST['email'],
+        password = pw_hash,
+    )
+    errors = User.objects.my_validator(request.POST)
+    c = User.objects.last()
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        c.delete()
+        return redirect ('/')
+    # request.session['email']
+    return redirect('/success')
+
+def login(request):
+    user = User.objects.filter(email = request.POST['login_email'])
+    if user:
+        logged_user = user[0]
+        if bcrypt.checkpw(request.POST['login_pw'].encode(), logged_user.password.encode()):
+            request.session['userid'] = logged_user.id
+            return redirect('/success')
+        else:
+            # errors = {}
+            # errors['incorrect_login'] = "Incorrect email or password"
+            messages.error(request, "Incorrect email or password")
+        return redirect('/')
+    return redirect('/')
+    
+
+def success(request):
+    context = {
+        'user': User.objects.first()
+    }
+    request.session['email'] = User.objects.first().email
+    print(User.objects.first().email)
+    return render(request, "success.html", context)
+
+def logout(request):
+    pass
