fdc25e2fc99fb842e80a6702040e64bfb3af5f99
Rene Montoya <rmontoya.we@gmail.com>
2020-12-11 17:16:41 -0800

    with help of sols

--- login_app/views.py
+++ login_app/views.py
@@ -11,15 +11,8 @@ def index(request):
     return render(request, "index.html", context)
 
 def register(request):
-    password = request.POST['password']
-    pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
-    print('hash', User.objects.all())
-    User.objects.create(
-        first_name = request.POST['first_name'],
-        last_name = request.POST['last_name'],
-        email = request.POST['email'],
-        password = pw_hash,
-    )
+    if request.method == "GET":
+        return redirect('/')
     errors = User.objects.my_validator(request.POST)
     c = User.objects.last()
     if len(errors) > 0:
@@ -27,31 +20,45 @@ def register(request):
             messages.error(request, value)
         c.delete()
         return redirect ('/')
-    # request.session['email']
-    return redirect('/success')
+    else:
+        password = request.POST['password']
+        pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        print('hash', User.objects.all())
+        new_user = User.objects.create(
+            first_name = request.POST['first_name'],
+            last_name = request.POST['last_name'],
+            email = request.POST['email'],
+            password = pw_hash,
+        )
+        request.session['user_id'] = new_user.id
+        return redirect('/success')
 
 def login(request):
-    user = User.objects.filter(email = request.POST['login_email'])
+    if request.method == "GET":
+        return redirect('/')
+    user = User.objects.filter(email = request.POST['email'])
+    print('email', user[0].email)
     if user:
         logged_user = user[0]
         if bcrypt.checkpw(request.POST['login_pw'].encode(), logged_user.password.encode()):
-            request.session['userid'] = logged_user.id
+            request.session['user_id'] = logged_user.id
             return redirect('/success')
         else:
-            # errors = {}
-            # errors['incorrect_login'] = "Incorrect email or password"
             messages.error(request, "Incorrect email or password")
-        return redirect('/')
-    return redirect('/')
-    
+        return redirect('/')    
 
 def success(request):
+    if 'user_id' not in request.session:
+        print("not in session")
+        return redirect('/')
+    user = User.objects.get(id = request.session['user_id'])
     context = {
-        'user': User.objects.first()
+        'user': user
     }
-    request.session['email'] = User.objects.first().email
-    print(User.objects.first().email)
+    request.session['email'] = user.email
+    print(user.email)
     return render(request, "success.html", context)
 
 def logout(request):
-    pass
+    request.session.clear()
+    return redirect('/')
